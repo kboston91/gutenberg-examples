adff8c71a0f36a33d4273552d926ab39401f1a70
Kourtney Boston <86276317+kboston91@users.noreply.github.com>
2022-06-05 16:47:59 -0500

    turned recipe title from h2 to h1

--- 05-recipe-card-esnext/build/index.asset.php
+++ 05-recipe-card-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ad0394762c8d935290a4906797c13406');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5007dcc3ee44cf103d3f');
